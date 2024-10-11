6b0c25d09f19924a222cb3f7c5b25b8a8d3eca53
Xerx81 <ryuvraj832@gmail.com>
2024-10-11 12:07:44 +0530

    added models and connected to mysql db

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.PositiveIntegerField()
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name} on {self.booking_date.strftime('%Y/%m/%d at %H:%M')}"
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.title}: ${self.price}"
